1eabf8e95ac3ada38cb3c1a5bbe2d5755a5412bb
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2021-10-25 20:53:23 -0400

    added app.js code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Switch, Route } from "react-router";
 import Navigation from "./components/Navigation";
 
@@ -6,18 +5,24 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
-/* TODO: Import the auth and list context providers from their respective files */
+import { AuthContextProvider } from "./context/AuthContext";
+import { ListContextProvider } from "./context/ListContext";
+
+import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
-			{/* TODO: Wrap the following jsx in the providers */}
-			<Navigation />
-			<Switch>
-				<Route path="/" exact component={Home} />
-				<Route path="/login" exact component={Login} />
-				<Route path="/register" exact component={Signup} />
-			</Switch>
+			<AuthContextProvider>
+				<ListContextProvider>
+					<Navigation />
+					<Switch>
+						<Route path="/" exact component={Home} />
+						<Route path="/login" exact component={Login} />
+						<Route path="/register" exact component={Signup} />
+					</Switch>
+				</ListContextProvider>
+			</AuthContextProvider>
 		</div>
 	);
 }
